dc7499ebda536f3f8f51d414c9029d1e9c37d3f4
khjj07 <khjj07@gmail.com>
2021-06-23 19:26:21 +0900

    initial setting

---
+++ game/hand/hand.go
@@ -0,0 +1,15 @@
+components {
+  id: "hand"
+  component: "/game/hand/hand.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
