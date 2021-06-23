986987005ebd257a3baf954ed133517b56ccaec2
xornjswldh <xornjswldh@naver.com>
2021-06-23 20:25:13 +0900

    character_manager generate

---
+++ game/character_manager/character_manager.go
@@ -0,0 +1,15 @@
+components {
+  id: "character_manager"
+  component: "/game/character_manager/character_manager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
