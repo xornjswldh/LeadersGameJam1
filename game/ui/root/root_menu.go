263be9d261b41d8e65dec52e444ca3a034321a45
khjj07 <khjj07@gmail.com>
2021-06-24 22:01:49 +0900

    card box error

---
+++ game/ui/root/root_menu.go
@@ -0,0 +1,51 @@
+components {
+  id: "root_menu"
+  component: "/game/ui/root/root_menu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "card_list"
+  type: "factory"
+  data: "prototype: \"/game/ui/card List/card_list.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "character_list"
+  type: "factory"
+  data: "prototype: \"/game/ui/character list/character_list.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
