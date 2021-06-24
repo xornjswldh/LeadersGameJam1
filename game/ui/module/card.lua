2d3e7464c26c07d5cb7cf47d66e32811288f0cd0
khjj07 <khjj07@gmail.com>
2021-06-25 03:31:59 +0900

    card list

--- game/ui/module/card.lua
+++ game/ui/module/card.lua
@@ -1,7 +1,22 @@
 local C ={}
 local CARD_MANAGER ="ingame:/card_manager"
-function C.create(card,pos,character,title,effect,cost,hold)
-	local new_card={node=card,pos=pos,character=character,title=title,effect=effect,cost=cost,hold=false}
+local COLOR = {
+	["군인"]={vmath.vector4(0,0.3,0,1),vmath.vector4(0.2,0.5,0.2,1),vmath.vector4(0.7,1,0.7,1)},
+	["과학자"]={vmath.vector4(1,1,0,1),vmath.vector4(1,1,0.5,1),vmath.vector4(1,1,0.7,1)},
+	["경찰"]={vmath.vector4(0,0,1,1),vmath.vector4(0.5,0.5,1,1),vmath.vector4(0.7,0.7,1,1)},
+	["도박사"]={vmath.vector4(1,0,0,1),vmath.vector4(1,0.5,0.5,1),vmath.vector4(1,0.7,0.7,1)},
+	["중립"]={vmath.vector4(1,1,1,1),vmath.vector4(1,1,1,1),vmath.vector4(1,1,1,1)}
+}
+function C.create(node,card,pos,character,title,effect,cost,hold)
+	local new_card={root=node,node=card,pos=pos,character=character,title=title,effect=effect,cost=cost,hold=false}
+	function new_card:initialize()
+		gui.set_color(new_card.root[hash("card_sample/inner_frame")], COLOR[new_card.character][2])
+		gui.set_color(new_card.root[hash("card_sample/explain_area")], COLOR[new_card.character][3])
+		gui.set_color(new_card.root[hash("card_sample/title_area")], COLOR[new_card.character][3])
+		gui.set_color(new_card.node, COLOR[new_card.character][1])
+		gui.set_text(new_card.root[hash("card_sample/explain")],new_card.effect)
+		gui.set_text(new_card.root[hash("card_sample/title")],new_card.title)
+	end
 	function new_card:change_position(pos)
 		gui.set_position(new_card.node, pos)
 	end
@@ -22,7 +37,7 @@ function C.create(card,pos,character,title,effect,cost,hold)
 		gui.animate(new_card.node, "rotation",  gui.get_rotation(new_card.pos), go.EASING_LINEAR, 0.1)
 	end
 	function new_card:use()
-		msg.post(CM, "use_card",{character=new_card.character,title=new_card.title,effect=new_card.effect,cost=new_card.cost})
+		msg.post(CARD_MANAGER, "use_card",{character=new_card.character,title=new_card.title,effect=new_card.effect,cost=new_card.cost})
 		new_card:change_position(vmath.vector3(-1000,-1000,0))
 		timer.delay(0.1, false, function()
 			gui.delete_node(new_card.node)
