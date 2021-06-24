579507a41d9af7dd7dc4ff12696b4afd4ca9fcca
xornjswldh <xornjswldh@naver.com>
2021-06-24 19:07:13 +0900

    character list card area

---
+++ game/ui/character list/character_list.go
@@ -0,0 +1,15 @@
+components {
+  id: "character_list"
+  component: "/game/ui/character list/character_list.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
